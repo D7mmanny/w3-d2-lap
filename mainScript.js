78fee69233ac1b52580f40334195905d2ce03f6e
D7mmanny <d7mmanny@gmail.com>
2023-08-07 15:21:59 +0300

    v1

---
+++ mainScript.js
@@ -0,0 +1,39 @@
+
+// q1
+for (let i = 0; i <= 10; i++) {
+    console.log(Math.pow(i, 2));
+
+    
+}
+
+// q2
+for (let j = 0; j < 20; j++) {
+    if(j%2==0){
+        
+    }else{
+        console.log("odd",j);
+    }
+    
+}
+
+// q3
+ for (let j = 0; j < 25; j++) {
+        if(j%2==0){
+            console.log("even",j);
+        }else{
+            
+        }
+        
+    }
+
+// q4
+let t = 8;
+for (let i = 1; i <= t; i++) {
+  let aa = "";
+  for (let j = 1; j <= i; j++) {
+    aa += j + "";
+  }
+  console.log(aa);
+}
+
+
